c0614604569fdad68c0a521512ad3f5fd5fcae66
AhmadDzulfikar <dzulfikar19.ahmad@gmail.com>
2022-05-20 11:15:32 +0700

    gabungin

--- home/admin.php
+++ home/admin.php
@@ -38,58 +38,6 @@ $ssaldo = $smasuk - $skeluar;
 
 
 
-<<<<<<< HEAD
-<!-- /.card -->
-
-
-<!-- /.card -->
-</div>
-<!-- /.col-md-6 -->
-<div class="col-lg-6">
-  <div class="card">
-    <div class="card-header border-0">
-      <div class="d-flex justify-content-between">
-        <h3 class="card-title">Rekap Kas Masjid</h3>
-        <a href="?page=rekap_km" class="nav-link">View Report</a>
-      </div>
-    </div>
-    <div class="card-body">
-      <div class="d-flex">
-        <p class="d-flex flex-column">
-          <span class="text-bold text-lg">Rp 2.775.000,00</span>
-          <span>Sales Over Time</span>
-        </p>
-        <p class="ml-auto d-flex flex-column text-right">
-          <span class="text-success">
-            <i class="fas fa-arrow-up"></i> 33.1%
-          </span>
-          <span class="text-muted">Since last month</span>
-        </p>
-      </div>
-      <!-- /.d-flex -->
-
-      <div class="position-relative mb-4">
-        <canvas id="sales-chart" height="200"></canvas>
-      </div>
-
-      <div class="d-flex flex-row justify-content-end">
-        <span class="mr-2">
-          <i class="fas fa-square text-primary"></i> Kas Masjid
-        </span>
-
-        <span>
-          <i class="fas fa-square text-gray"></i> Kas Sosial
-        </span>
-      </div>
-    </div>
-  </div>
-  <!-- /.card -->
-
-
-  <!-- /.col-md-6 -->
-</div>
-<script src="plugins/jquery/jquery.min.js"></script>
-=======
                 <div class="position-relative mb-4">
                   <canvas id="deleps-chart" height="200"></canvas>
                 </div>
@@ -154,7 +102,6 @@ $ssaldo = $smasuk - $skeluar;
           <!-- /.col-md-6 -->
         </div>
 		<script src="plugins/jquery/jquery.min.js"></script>
->>>>>>> bff81424ad0dfdb1dc10406af41e656fed8258a4
 <!-- Bootstrap -->
 <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 
